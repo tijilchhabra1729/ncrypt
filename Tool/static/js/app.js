a7a2293176a3d9830b40f949497b9acc04c07a67
Moaksh <moakshkakar@gmail.com>
2020-10-09 17:44:56 +0530

    Reg_form_change

--- Tool/static/js/app.js
+++ Tool/static/js/app.js
@@ -18,4 +18,36 @@ $(window, document, undefined).ready(function () {
 });
 
 
+
+// You can modify the upload files to pdf's, docs etc
+//Currently it will upload only images
+
+$(document).ready(function ($) {
+
+    // Upload btn on change call function
+    $(".uploadlogo").change(function () {
+        var filename = readURL(this);
+        $(this).parent().children('span').html(filename);
+    });
+
+    // Read File and return value  
+    function readURL(input) {
+        var url = input.value;
+        var ext = url.substring(url.lastIndexOf('.') + 1).toLowerCase();
+        if (input.files && input.files[0] && (
+            ext == "png" || ext == "jpeg" || ext == "jpg" || ext == "gif" || ext == "pdf"
+        )) {
+            var path = $(input).val();
+            var filename = path.replace(/^.*\\/, "");
+            // $('.fileUpload span').html('Uploaded Proof : ' + filename);
+            return "Uploaded file : " + filename;
+        } else {
+            $(input).val("");
+            return "Only image/pdf formats are allowed!";
+        }
+    }
+    // Upload btn end
+
+});
+
 console.log('Xino <3 Ncrypt')
